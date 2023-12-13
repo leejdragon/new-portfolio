93a51fac38a3c9c50553848cfe9ffda7f6f6dfb5
leejdragon <leejdragon@naver.com>
2023-12-13 16:53:42 +0900

    메인 스크립트 작업

--- portfolio/common/index.js
+++ portfolio/common/index.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
+
+
+// let progressBar = document.querySelector(".skills_html");
+
+let progressBar = document.querySelectorAll('.skills_bar');
+
+window.addEventListener("scroll", function (){
+  let value = window.scrollY;
+  // console.log("scrollY", value);
+
+  if(value > 350){
+    for (let i = 0; i < progressBar.length; i++) {
+      progressBar[i].style.animation = "progress 1.5s ease-in-out forwards";
+    };
+  };
+});
+
+// bxSlider 
+$(document).ready(function () {
   $('.slider').bxSlider({
     mode: 'fade',
     speed: 1000,
