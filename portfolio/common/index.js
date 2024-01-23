e2e0a7d9c2b4ca5e65456202f521b3a73e101219
leejdragon <leejdragon@naver.com>
2024-01-23 20:38:46 +0900

    포트폴리오 작업

--- portfolio/common/index.js
+++ portfolio/common/index.js
@@ -1,10 +1,10 @@
 let progressBar = document.querySelectorAll('.skills_bar');
 
-window.addEventListener("scroll", function (){
+window.addEventListener("scroll", function () {
   let value = window.scrollY;
   // console.log("scrollY", value);
 
-  if(value > 350){
+  if (value > 350) {
     for (let i = 0; i < progressBar.length; i++) {
       progressBar[i].style.animation = "progress 1.5s ease-in-out forwards";
     };
@@ -13,12 +13,12 @@ window.addEventListener("scroll", function (){
 
 let scale = document.querySelector('.skills_img img');
 
-window.addEventListener("scroll", function (){
+window.addEventListener("scroll", function () {
   let value = window.scrollY;
   // console.log("scrollY", value);
 
-  if(value > 293){
-    scale.style.animation = "scale 1.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both";
+  if (value > 373) {
+    scale.style.animation = "scale 2.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both";
   };
 });
 
@@ -38,4 +38,87 @@ $(document).ready(function () {
 $('.top').click(function () {
   $('html, body').animate({ scrollTop: 0 }, 500);
   return false;
+});
+
+// 플로팅 배너
+$(document).ready(function () {
+
+  var floatPosition = parseInt($(".sideban").css('top'));
+
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    var newPosition = scrollTop + floatPosition + "px";
+
+    $(".sideban").stop().animate({
+      "top": newPosition
+    }, 500);
+
+  }).scroll();
+
+});
+
+
+// $(".collapse-item").click(function(){
+//   if($(this).find(".content").is(":visible")){
+//       $(this).find(".content").slideUp();
+//   }
+//   else{
+//       $(this).find(".content").slideDown();
+//   }
+// });
+
+
+// $(document).ready(function(){
+//   $(".menu > p").click(function(){
+//     var submenu = $(this).next("ul");
+
+//     if( submenu.is(":visible") ){
+//       submenu.slideUp();
+//     }else{
+//       submenu.slideDown();
+//     }
+//   });
+// });
+
+
+
+$(document).ready(function () {
+  $(".title").click(function () {
+    if ($(this).hasClass("active")) {
+      slideUp();
+    } else {
+      slideUp();
+      $(this).addClass("active").next().slideDown();
+    }
+    function slideUp() {
+      $(".title").removeClass("active").next().slideUp();
+    }
+  });
+});
+
+
+
+$(document).ready(function(){
+ 
+//   $('.hg_btn').click(function (){
+//       $('.nav_menu').show();
+//   });
+
+//  $('.hg_btn').click(function (){
+//       $('.nav_menu').hide();
+//   });
+
+// $('.hg_btn').click(function (){
+//       $('#header').toggleClass('on');
+//   });
+//   $('.close').click(function (){
+//       $('#header').removeClass('on');
+//   });
+
+  $('.more_btn').click(function() {
+    $(this).toggleClass('active');
+    $('.nav_list').toggleClass('active');
+  });
+
+  
 });
